2b6f91e10b0ff63160c414181d5cc71b5d3c614b
Markus Mayer <widemeadows@gmail.com>
2020-06-20 18:38:12 +0200

    Add reverse lookup of cell to group

--- lib/sudoku/src/board.rs
+++ lib/sudoku/src/board.rs
@@ -5,26 +5,32 @@ use visitor::{Visitor, AcceptVisitor};
 use std::collections::HashSet;
 use std::vec::Vec;
 use std::collections::hash_map::RandomState;
+use std::rc::Rc;
+use std::mem::MaybeUninit;
 
 pub type Cell = Option<u8>;
+type IndexSet = HashSet<usize, RandomState>;
 
 pub struct Board {
     width: usize,
     height: usize,
     cells: [Cell; 9 * 9],
-    groups: Vec<HashSet<usize, RandomState>>
+    groups: Vec<Rc<IndexSet>>,
+    group_lookup: [Rc<IndexSet>; 9 * 9]
 }
 
 impl Board {
     /// Initializes a standard Sudoku board from values in row-major order.
-    pub fn new(cells: [Cell; 9 * 9]) -> Board {
-        let mut groups = build_set_of_default_sets();
-        Board { width: 9, height: 9, cells, groups }
+    pub fn new(cells: [Cell; 81]) -> Board {
+        let groups = build_set_of_default_groups();
+        let group_lookup = build_default_index_to_group_lookup(&groups);
+        Board { width: 9, height: 9, cells, groups, group_lookup }
     }
 
     pub fn new_empty() -> Board {
-        let mut groups = build_set_of_default_sets();
-        Board { width: 9, height: 9, cells: [None; 81], groups }
+        let groups = build_set_of_default_groups();
+        let group_lookup = build_default_index_to_group_lookup(&groups);
+        Board { width: 9, height: 9, cells: [None; 81], groups, group_lookup }
     }
 
     pub fn cell(&self, x: usize, y: usize) -> Cell {
@@ -43,31 +49,46 @@ fn index(x: usize, y: usize, width: usize) -> usize {
     x + y * width
 }
 
-fn build_default_set(x_offset: usize, y_offset: usize, width: usize) -> HashSet<usize, RandomState> {
+/// Builds a default group rooted at the specified offsets.
+fn build_default_group(x_offset: usize, y_offset: usize) -> Rc<IndexSet> {
     let mut set = HashSet::new();
-    for y in 0..3 {
-        set.insert(index(0 + x_offset, y + y_offset, width));
-        set.insert(index(1 + x_offset, y + y_offset, width));
-        set.insert(index(2 + x_offset, y + y_offset, width));
+    for y in (0 + y_offset)..(3 + y_offset) {
+        set.insert(index(0 + x_offset, y, 9));
+        set.insert(index(1 + x_offset, y, 9));
+        set.insert(index(2 + x_offset, y, 9));
     }
-    set
+    Rc::new(set)
 }
 
-fn build_set_of_default_sets() -> Vec<HashSet<usize, RandomState>> {
+/// Builds the set of default groups.
+fn build_set_of_default_groups() -> Vec<Rc<IndexSet>> {
     let mut groups = Vec::new();
     for y in (0..9).step_by(3) {
         for x in (0..9).step_by(3) {
-            let set = build_default_set(x, y, 9);
+            let set = build_default_group(x, y);
             groups.insert(groups.len(), set);
         }
     }
     groups
 }
 
+/// Builds a reverse index of each cell to its group.
+fn build_default_index_to_group_lookup(groups: &Vec<Rc<IndexSet>>) -> [Rc<IndexSet>; 81] {
+    let mut group_lookup: [MaybeUninit<Rc<IndexSet>>; 81] = unsafe { MaybeUninit::uninit().assume_init() };
+
+    for group in groups {
+        for index in group.iter() {
+            group_lookup[*index] = MaybeUninit::new(group.clone());
+        }
+    }
+
+    unsafe { std::mem::transmute::<_, [Rc<IndexSet>; 81]>(group_lookup) }
+}
+
 #[cfg(test)]
 mod tests {
     use std::mem::MaybeUninit;
-    use crate::board::{build_default_set, index};
+    use crate::board::{build_default_group, index};
 
     fn create_matrix() -> [crate::Cell; 81] {
         let mut array: [MaybeUninit<crate::Cell>; 81] = unsafe { MaybeUninit::uninit().assume_init() };
@@ -98,7 +119,7 @@ mod tests {
     fn elements_in_group() {
         for y in (0usize..9).step_by(3) {
             for x in (0usize..9).step_by(3) {
-                let set = build_default_set(x, y, 9);
+                let set = build_default_group(x, y);
                 assert_eq!(set.contains(&index(0 + x, 0 + y, 9)), true);
                 assert_eq!(set.contains(&index(0 + x, 2 + y, 9)), true);
                 assert_eq!(set.contains(&index(2 + x, 2 + y, 9)), true);
@@ -110,10 +131,22 @@ mod tests {
     fn elements_not_in_group() {
         for y in (0usize..9).step_by(3) {
             for x in (0usize..9).step_by(3) {
-                let set = build_default_set(x, y, 9);
+                let set = build_default_group(x, y);
                 assert_eq!(set.contains(&index(0 + x, 3 + y, 9)), false);
                 assert_eq!(set.contains(&index(3 + x, 0 + y, 9)), false);
             }
+        }
+    }
+
+    #[test]
+    fn group_lookup_works() {
+        let board = crate::Board::new(create_matrix());
+        let group = board.group_lookup[index(4, 4, 9)].clone();
+
+        for y in 3..6 {
+            assert!(group.contains(&index(3, y, 9)));
+            assert!(group.contains(&index(4, y, 9)));
+            assert!(group.contains(&index(5, y, 9)));
         }
     }
 }
